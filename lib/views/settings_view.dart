e1056e1befb9fbd63307f645038bee04c93b740d
Krantikuma <107709433+Krantikuma@users.noreply.github.com>
2025-03-02 23:08:30 +0530

    setting ui for news app

---
+++ lib/views/settings_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.rx.dart';
+import '../controllers/settings_controller.dart';
+
+class SettingsView extends StatelessWidget {
+  final SettingsController settingsController = Get.find<SettingsController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: ListView(
+        children: [
+          Obx(
+                () => SwitchListTile(
+              title: Text('Dark Mode'),
+              value: settingsController.isDarkMode.value,
+              onChanged: settingsController.toggleDarkMode,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
